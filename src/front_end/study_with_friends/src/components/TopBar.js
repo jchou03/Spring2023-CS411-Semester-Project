4f882f4c6dcf04c6b95da6862270b10901cdf1d1
jchou21 <jared.chou7@gmail.com>
2023-03-30 23:05:37 -0400

    added custom topbar sign in/out display

--- src/front_end/study_with_friends/src/components/TopBar.js
+++ src/front_end/study_with_friends/src/components/TopBar.js
@@ -1,16 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 import '../style/App.css';
 import '../style/TopBar.css'
 import Button from 'react-bootstrap/Button'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function TopBar (props) {
-    var username = "YDD Laz"
+    const [username, setUsername] = useState("YDD Laz")
+
+    const signOut = () => {
+        setUsername("")
+    }
+
+    const signIn = () => {
+        setUsername("YDD Laz")
+    }
+
     return (
         <div id="topbar">
             <h1>Study Finder</h1>
             <div>
                 {/* can add a profile picture in top right later */}
-                {username === "" ? (<Button variant="primary">Sign In</Button>) : (<div class="right-side"><p>Hello {username}</p> <Button variant="Primary">Sign Out</Button></div>)}
+                {/* custom right side depending on if the user is signed in or not */}
+                {username === "" ? 
+                    (<Button variant="primary" onClick={signIn}>Sign In</Button>) : 
+                    (<div class="right-side">
+                        <p>Hello {username}</p> 
+                        <Button variant="primary" onClick={signOut}>Sign Out</Button>
+                    </div>)
+                }
             </div>
         </div>
     )

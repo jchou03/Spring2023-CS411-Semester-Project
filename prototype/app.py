a33b7f442f8396f171b50cdc776acf3a47d19f48
boowenja <113324252+boowenja@users.noreply.github.com>
2023-04-17 05:58:16 -0400

    Update app.py

--- prototype/app.py
+++ prototype/app.py
@@ -7,10 +7,105 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
    return render_template('index.html')
-# app.add_url_rule('/', 'hello', hello_world)这个是重定向  就是把hello这个路由映射成/，所以你看上去没变化
+
+#Database connection
+def connect_to_db():
+    return mysql.connector.connect(
+        user='root',
+        host='127.0.0.1',
+        port=3306,
+        password='Hikeleftstation12',
+        database='app_server'
+    )
+
+#Test version
+def add_user(user_name, user_id, user_instagram_connection, user_custom_id):
+    db = connect_to_db()
+    cursor = db.cursor()
+
+    sql = ('''INSERT INTO user_profile(user_name, user_id, user_instagram_connection, user_custom_id) VALUES (%s, %s, %s, %s)''')
+    val = (user_name, user_id, user_instagram_connection, user_custom_id)
+
+    cursor.execute(sql, val)
+    db.commit()
+
+    print(cursor.rowcount, "record inserted.")
+    cursor.close()
+    db.close()
+      
+      
+
+def add_friend(user_id, friend_id, friend_relation):
+    db = connect_to_db()
+    cursor = db.cursor()
+
+    sql = ('''INSERT INTO friend_list(user_id, friend_id, friend_relation) VALUES (%s, %s, %s)''')
+    val = (user_id, friend_id, friend_relation)
+
+    cursor.execute(sql, val)
+    db.commit()
+
+    print(cursor.rowcount, "record inserted.")
+    cursor.close()
+    db.close()
+
+   
+def find_all_friends(user_id):
+    db = connect_to_db()
+    cursor = db.cursor()
+
+    query = ('''SELECT * FROM friend_list WHERE user_id = %s OR friend_id = %s;''' % (user_id, user_id))
+    cursor.execute(query)
+
+    ls = []
+
+    for info in cursor:
+        ls.append(info)
+
+    cursor.close()
+    db.close()
+    return ls
+
+def find_user(user_id):
+    db = connect_to_db()
+    cursor = db.cursor()
+
+    query = ('''SELECT * FROM user_profile WHERE user_id = %s;''' % (user_id))
+    cursor.execute(query)
+
+    ls = []
+
+    for info in cursor:
+        ls.append(info)
+
+    cursor.close()
+    db.close()
+    return ls
+
+@app.route('/add_user', methods=['POST'])
+def add_user_route():
+    user_data = request.get_json()
+    add_user(
+        user_data['user_name'],
+        user_data['user_id'],
+        user_data['user_instagram_connection'],
+        user_data['user_custom_id']
+    )
+    return jsonify({"message": "User added successfully"}), 201
+   
+ 
+@app.route('/find_all_friends/<int:user_id>', methods=['GET'])
+def find_all_friends_route(user_id):
+    friends = find_all_friends(user_id)
+    return jsonify(friends)
 
 
-\
+@app.route('/remove_user/<int:user_id>', methods=['DELETE'])
+def remove_user_route(user_id):
+    remove_user(user_id)
+    return jsonify({"message": "User removed successfully"}), 200
+
+
 @app.route('/login')
 def login():
    username = request.form.get("username")

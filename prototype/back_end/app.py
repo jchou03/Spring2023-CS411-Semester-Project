1bcb7ec4c6b6d3a03ef716769f5ea6f478673368
spencer104 <121702834+spencer104@users.noreply.github.com>
2023-04-26 21:53:41 -0400

    Donham Fun Facts

--- prototype/back_end/app.py
+++ prototype/back_end/app.py
@@ -16,6 +16,15 @@ def get_conn():
     return mysql.connector.connect(user='root', host='127.0.0.1', port=3306, password='Hikeleftstation12', database='app_server')
     
 def add_user(user_name, user_id, user_instagram_connection, user_custom_id, user_email,  user_password, user_location, user_study_time, is_user_studying, json_object):
+   # add_user is a function that allows us to add users to our database 
+   # the User_id is our primary key and each entry must have a unique user_id 
+   # while every other entry is optional, but please fill it in plz pretty plz 
+   # -Spencer Yeh 
+   # 
+   # Donham Fun Fact: the Rafik B. Hariri where Questrom is located is named after the Former Prime Minister of Lebanon.
+   # Hariri was accused of corruption that plagued Lebanon during the Syrian occupation. Among the allegations made against him was that
+   # his wealth grew from less than $1 billion when he was appointed prime minister in 1992, to over $16 billion when he died.
+    
     db = get_conn()
 
     cursor = db.cursor()
@@ -36,12 +45,28 @@ def add_user(user_name, user_id, user_instagram_connection, user_custom_id, user
 
     db.close()
 
-#ENUM('1 friend 2' , '2 friend 1', 'friend both', '1 block 2', '2 block 1', 'block both' )
+
 
 def add_friend(user_id, friend_id, friend_relation):
-    # RULES 
-    # USER_id > friend_id 
-    # no two of the same records 
+    # this function adds user relationships to our user Friend List 
+    # below are a few rules how our friends list works 
+    #
+    # 1. There are 6 options for a friend relation,  
+    #'1 friend 2' , '2 friend 1', 'friend both', '1 block 2', '2 block 1', 'block both'
+    # 
+    # 2. USER_id > friend_id 
+    # the user_id must be larger than friend_id therefore we get no duplicates 
+    # 
+    # in theory user_id can equal friend_id, but dont be stupid and do that 
+    # 
+    # - Spencer Yeh 
+    # 
+    # Donham Fun Fact: The Yawkey building in Kenmore is named after Thomas Yawkey president of the Boston Red Sox in 1933 for 44 seasons 
+    # The Red Sox were the first MLB team to sign a Mexican-born player, fielding Mel Almada on September 8, 1933. However, they were 
+    # the last major league team to add a black player to their roster allegedly due to Yawkey and the managers he hired being racists. 
+    # These claims have been disputed by some journalists and researchers. In his biography of Yawkey, Bill Nowlin states that there is 
+    # no evidence that Yawkey ever made a racist statement or was "personally racist." Furthermore, a 2006 article in The Boston Globe 
+    # commented that Yawkey "was not overtly racist, but members of his inner circle were."
 
     db = get_conn()
 
@@ -64,9 +89,22 @@ def add_friend(user_id, friend_id, friend_relation):
     db.close()
 
 def find_all_friends(user_id):
-    # 
-    # Input user id to return all user relationships 
+    # This function finds all the friends a certain user has, if they have any
     #
+    # Input the user id and it will output a list of tuples of all the friends and their relationship status  
+    # Hopefully pretty simple 
+    #
+    # -Spencer Yeh 
+    #
+    # Donham Fun Fact: The Law building was named after Sumner M Redstone was an American billionaire businessman and media magnate. 
+    # He was the founder and chairman of the second incarnation of Viacom, chairman of CBS Corporation,and the majority owner and chairman
+    # of the National Amusements theater chain. He also taught law at BU for a bit. 
+    # In July 2010, Redstone was caught on tape trying to find the source of an apparently embarrassing leak within MTV.
+    # Redstone offered money and protection to a journalist if he would give up his source. Redstone had been pushing MTV 
+    # management to give more airtime to the band the Electric Barbarellas. On the message, Redstone tells the reporter that 
+    # "we're not going to kill" the source, adding "We just want to talk to him". The 87-year-old Redstone also told the reporter 
+    # he would be "well rewarded and well protected" if he would reveal the source. 
+
     db = get_conn()
 
     cursor = db.cursor()
@@ -89,9 +127,16 @@ def find_all_friends(user_id):
     return ls
 
 def find_user(user_id): 
-    #
-    # returns user info in a list
-    #
+    # This function allows us to find a user in the user profile list and returns their info in a list which in than is a in a tuple. 
+    # Enter a user_id that does exist, or else it will give an error/not return anything 
+    # 
+    # - Spencer Yeh 
+    # 
+    # Donham Fun Fact: Jon Westling was the Eighth President of Boston University from 1996 - 2002. 
+    # During the Court Case, Guckenberger v. Boston University, he was accused of making controversial statements 
+    # about students with learning disabilities. "President Westling referred to students with learning disabilities as "a plague," 
+    # and an indication of "a silent genetic catastrophe," and he has made similar statements in letters to the New York Times, 
+    # the Boston Globe, campus newspapers, and students' parents.
 
     db = get_conn()
 
